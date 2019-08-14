12e443d761e80eb257a700fadcc11c20015a5847
Samantha Acampora <samantha.acampora@gmail.com>
2019-08-14 00:47:08 -0400

    Login and out successfully implemented

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'welcome/about'
   root 'welcome#index'
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
